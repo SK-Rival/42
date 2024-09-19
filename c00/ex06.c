507a444e0af4c1369e75e9702f32f670c231df84
Rival <omer.tokail.pr@gmail.com>
2024-09-19 18:30:05 +0200

    c00
    ex06

--- c00/ex06.c
+++ c00/ex06.c
@@ -1,4 +1,29 @@
 #include <unistd.h>
 
-void ft_print_comb2(void)
+void	ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
 
+void	ft_print_comb2(void)
+{
+    char a;
+    char b;
+
+    for (a = 0; a <= 98; a++)
+    {
+        for (b = a + 1; b <= 99; b++)
+        {
+            ft_putchar(a / 10 + '0');
+            ft_putchar(a % 10 + '0');
+            ft_putchar(' ');
+            ft_putchar(b / 10 + '0');
+            ft_putchar(b % 10 + '0');
+            if (a != 98 || b != 99)
+            {
+                ft_putchar(',');
+                ft_putchar(' ');
+            }
+        }
+    }
+}
