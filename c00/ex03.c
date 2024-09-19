d48ba84e8e0c0a1d304c0f1f5e024d90263e0e5b
Rival <omer.tokail.pr@gmail.com>
2024-09-19 17:12:41 +0200

    c00
    ex03

--- c00/ex03.c
+++ c00/ex03.c
@@ -1,3 +1,14 @@
-//
-// Created by Cheetoh on 19-09-24.
-//
+#include <io.h>
+#include <unistd.h
+
+void ft_print_numbers(void);
+
+{
+    char n = 0;
+
+    while (n <= 9)
+    {
+        write(1, &n, 1);
+        n++;
+    }
+}
