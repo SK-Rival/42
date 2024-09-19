97997294b46b1b2545153fc6c3cd1eea321df1c3
Rival <omer.tokail.pr@gmail.com>
2024-09-19 17:51:06 +0200

    c00
    ex05

--- c00/ex05.c
+++ c00/ex05.c
@@ -1,4 +1,26 @@
 #include <unistd.h>
 
 void ft_print_comb(void)
+{
+    char a;
+    char b;
+    char c;
 
+    for (a = '0';  a<= '7'; a++)
+    {
+        for (b = a + 1; b <= '8'; b++)
+        {
+            for (c = b + 1; c <= '9'; c++)
+            {
+                write(1, &a, 1);
+                write(1, &b, 1);
+                write(1, &c, 1);
+
+                if (a != '7' || b != '8' || c != '9')
+                {
+                    write(1, ", ", 2);
+                }
+            }
+        }
+    }
+}
